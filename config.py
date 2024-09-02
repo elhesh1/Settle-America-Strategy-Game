1fe10a1a16d5ff82e7fb54f7be5fa00496b8a48c
Elliot Sherman <ehs4@illinois.edu>
2024-09-02 00:27:36 -0500

    moved DBs

--- config.py
+++ config.py
@@ -3,27 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flask_cors import CORS
 import logging
-
-# app = Flask(__name__)
-# app.url_map.strict_slashes = False
-# CORS(app) 
-
+import os
 
 app = Flask(__name__, template_folder='templates') 
 app.url_map.strict_slashes = False
 CORS(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('postgres://u63krlnclqemjd:peb1fe8889ba5f449883feba1fc4a11d8b7f7c9ce5407faa0dc476ba30d014d08@c5hilnj7pn10vb.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d58gfiaefviqgf', 'sqlite:///mydatabase.db')
 
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-
 log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)  
+log.setLevel(logging.ERROR)
 
-
-db = SQLAlchemy(app) 
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
